ceac8be05bb2b9ea0e8413f22e51a56deb48bd17
chanhi <61691088+chanhi@users.noreply.github.com>
2021-10-10 14:28:09 +0000

    i want to add URL control

--- README.md
+++ README.md
@@ -1,3 +1,9 @@
 this is my first web crawler making by python
 ### NOMARD CODER
 ### DO it! 파이썬 생활 프로그래밍
+
+BeautifulSoup
+>find_all()
+>find()
+
+(TAG,{CLASS:NAME})[UNDER_TAG]
--- quotes.py
+++ quotes.py
@@ -1,12 +1,13 @@
-import requests
 import urllib.request as ur
 #import os, re, usecsv
 from bs4 import BeautifulSoup as bs
 
-URL = 'https://quotes.toscrape.com/'
+TAG = 'tag/love/page/'
+PAGE = 1
+URL = f'https://quotes.toscrape.com/{TAG}{PAGE}/'
 
 def get_quotes():
   soup = bs(ur.urlopen(URL).read(), 'html.parser')
-  quotes_list = soup.find_all('div', {'class':'quote'})
+  quotes_list = soup.find_all('span', {'class':'text'})
   return quotes_list
 
