2cbd407832f483ada17a8a4ae45baa4a231b6ecf
chanhi <61691088+chanhi@users.noreply.github.com>
2021-11-10 14:52:23 +0000

    ? try to revise indeed.py by its page update but didnt operate and make csv creater

--- indeed.py
+++ indeed.py
@@ -17,8 +17,8 @@ def get_last_page():
     return max_page
 
 def extract_job(html):
-    title = html.find("h2",{"class": "title"}).find("a")["title"]
-    company = html.find("span", {"class": "company"})
+    title = html.find("h2",{"class": "jobTitle"}).find("a")["title"]
+    company = html.find("span", {"class": "companyName"})
     if company:
       company_anchor = company.find("a")
       if company_anchor is not None:
@@ -28,9 +28,15 @@ def extract_job(html):
       company = company.strip()
     else:
       company = None
-    location = html.find("div",{"class": "recJobLoc"})["data-rc-loc"]
+    location = html.find("div",{"class": "companyLocation"}).string
     job_id = html["data-jk"]
-    return {'title': title,'company': company, 'location': location, "Link": f"https://www.indeed.com/viewjob?jk={job_id}"}
+    print(title, company, location)
+    return {
+      'title': title,
+      'company': company,
+      'location': location,
+      "Link": f"https://www.indeed.com/viewjob?jk={job_id}"
+    }
 
 def extract_jobs(last_page):
   jobs = []
@@ -38,7 +44,7 @@ def extract_jobs(last_page):
     print(f"Scrapping indeed page {page}")
     result = requests.get(f"{URL}&start={page*LIMIT}")
     soup = BeautifulSoup(result.text, "html.parser")
-    results = soup.find_all("div",{"class": "jobsearch-SerpJobCard"})
+    results = soup.find_all("a",{"class": "tabItem"})
     for result in results:
       job = extract_job(result)
       jobs.append(job)
--- main.py
+++ main.py
@@ -1,14 +1,18 @@
-#from indeed import get_jobs as get_indeed_jobs
-#from so import get_jobs as get_so_jobs
+from indeed import get_jobs as get_indeed_jobs
+from so import get_jobs as get_so_jobs
+from save import save_to_file
 #from quotes import get_quotes
-from news import find_hdline, test
+#from news import find_hdline, test
 
-#indeed_jobs = get_indeed_jobs()
+indeed_jobs = get_indeed_jobs()
+print(indeed_jobs)
 #so_jobs = get_so_jobs()
 #print(so_jobs)
+jobs = indeed_jobs# + so_jobs
+save_to_file(jobs)
 
 #for i in get_quotes():
 #  print(i.text)
 
-find_hdline()
+#find_hdline()
 #test()
